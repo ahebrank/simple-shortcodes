5798690e6988b891f327502bd25284bd028101a8
Andy Hebrank <ahebrank@gmail.com>
2015-09-09 22:03:49 -0400

    update accordion to strip cruft out of wysiwyg

--- codes/accordion.php
+++ codes/accordion.php
@@ -32,7 +32,17 @@ class accordion_shortcode extends plugin_shortcode {
   private function parse_accordion_interior() {
     $this->wrap_content('item', '<li class="accordion-navigation">', '</li>');
     $this->wrap_content('title', '<a href="#%s%d">', '</a>', array($this->params['id'], 'COUNTER'));
-    $this->wrap_content('content', '<div id="#%s%d">', '</div>', array($this->params['id'], 'COUNTER'));
+    $this->wrap_content('content', '<div id="%s%d" class="content">', '</div>', array($this->params['id'], 'COUNTER'));
+  
+    // strip out all the stuff between the tags
+    $this->interior = preg_replace('/^(.*)(<li)/sU', '${2}', $this->interior);
+    $this->interior = preg_replace('/(<li.*>).*(<a)/sU', '${1}${2}', $this->interior);
+    $this->interior = preg_replace('/(<a.*>.*<\\/a>).*(<div id.*>)<\\/p>/sU', '${1}${2}', $this->interior);
+    $this->interior = preg_replace('/(<div id.*>)(<\\/p>)/sU', '${1}', $this->interior);
+    $this->interior = preg_replace('/<p> *<\\/div>/sU', '</div>', $this->interior);
+    $this->interior = preg_replace('/<p> *<\\/li> *<\\/p>/sU', '</li>', $this->interior);
+    $this->interior = preg_replace('/(<\\/li>).*(<li.*>)/sU', '${1}${2}', $this->interior);
+    $this->interior = preg_replace('/<p>$/s', '${1}', $this->interior);
   }
 
 }
