d1c364f7e8dda120139623d2df90061fc0ac7426
Andy Hebrank <ahebrank@gmail.com>
2015-09-12 14:51:50 -0400

    fix accordion wysiwyg replacement

--- codes/accordion.php
+++ codes/accordion.php
@@ -30,19 +30,17 @@ class accordion_shortcode extends plugin_shortcode {
    * parse the interior for item title and content
    */
   private function parse_accordion_interior() {
+    // strip out all the stuff between the tags
+    $this->interior = preg_replace('/^(.*)(\\[item\\])/sU', '${2}', $this->interior);
+    $this->interior = preg_replace('/(\\[item\\]).*(\\[title\\])/sU', '${1}${2}', $this->interior);
+    $this->interior = preg_replace('/(\\[\\/title\\]).*(\\[content\\])/sU', '${1}${2}', $this->interior);
+    $this->interior = preg_replace('/(\\[\\/content\\]).*(\\[\\/item\\])/sU', '${1}${2}', $this->interior);
+    $this->interior = preg_replace('/(\\[\\/item\\]).*(\\[item\\])/sU', '${1}${2}', $this->interior);
+    $this->interior = preg_replace('/(\\[\\/item\\]).*(\\[\\/accordion\\])/sU', '${1}${2}', $this->interior);
+
     $this->wrap_content('item', '<li class="accordion-navigation">', '</li>');
     $this->wrap_content('title', '<a href="#%s%d">', '</a>', array($this->params['id'], 'COUNTER'));
     $this->wrap_content('content', '<div id="%s%d" class="content">', '</div>', array($this->params['id'], 'COUNTER'));
-  
-    // strip out all the stuff between the tags
-    $this->interior = preg_replace('/^(.*)(<li)/sU', '${2}', $this->interior);
-    $this->interior = preg_replace('/(<li.*>).*(<a)/sU', '${1}${2}', $this->interior);
-    $this->interior = preg_replace('/(<a.*>.*<\\/a>).*(<div id.*>)<\\/p>/sU', '${1}${2}', $this->interior);
-    $this->interior = preg_replace('/(<div id.*>)(<\\/p>)/sU', '${1}', $this->interior);
-    $this->interior = preg_replace('/<p> *<\\/div>/sU', '</div>', $this->interior);
-    $this->interior = preg_replace('/<p> *<\\/li> *<\\/p>/sU', '</li>', $this->interior);
-    $this->interior = preg_replace('/(<\\/li>).*(<li.*>)/sU', '${1}${2}', $this->interior);
-    $this->interior = preg_replace('/<p>$/s', '${1}', $this->interior);
   }
 
 }
