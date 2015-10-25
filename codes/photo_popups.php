7ecb43b40a6723e6053a4e31c458fc9520076335
Andy Hebrank <ahebrank@gmail.com>
2015-10-24 22:39:52 -0400

    photo popups list

---
+++ codes/photo_popups.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Photo popups are specified by:
+ *
+ * [photo_popups]
+ *   ( HTML list of links to images )
+ * [/photo_popups]
+ *
+ * Note: requires jQuery
+ * 
+ */
+class photo_popups_shortcode extends plugin_shortcode {
+
+  public function render() {
+    $this->parse_photo_popups();
+    $this->append_photo_popup_script();
+    return '<div class="photo-popup-wrapper">' . $this->interior . '</div>';
+  }
+
+  private function parse_photo_popups() {
+    preg_match_all('/<a (.*)>/U', $this->interior, $matches);
+    $replace = '<a %s data-photo-popup>';
+    for ($i = 0; $i < count($matches); $i++) {
+      $this->interior = str_replace($matches[0][$i], sprintf($replace, $matches[1][$i]), $this->interior);
+    }
+  }
+
+  private function append_photo_popup_script() {
+    $script = <<<EOS
+<script>
+  $('[data-photo-popup]').on({
+    'mouseenter': function(e) {
+      var div = $('<div class="photo-popup" style="position: absolute;">');
+      var img = $('<img width="200" src="' + $(this).attr('href') + '" alt="' + escape($(this).text()) + '">')
+      div.append(img);
+      var offset = $(this).offset();
+      div.css({
+        top: offset.top + 30,
+        left: offset.left + 30
+      });
+      $('body').append(div)
+    },
+    'mouseleave': function(e) {
+      $('body > .photo-popup').remove();
+    }
+  });
+</script>
+EOS;
+    $this->interior .= $script;
+  }
+
+}
